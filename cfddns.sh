b516d7889e21d0d5e78620bd2f73a77f41d417ba
Asif Bacchus <asif@programming.bacchus.network>
2018-09-11 23:32:42 -0600

    Setup initial skeleton for script with parameters

---
+++ cfddns.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+### Define functions
+
+function scriptHelp {
+echo -e "\e[1;31mInvalid parameter(s) provided\e[0m"
+echo -e "\e[1;39mUsage: \e[1;36m$(basename ${0})" \
+    "\e[1;35m-f path/to/account/details.file" \
+    "\e[1;33m-r record.to.update\e[0m" \
+    "\e[1;33m[-r another.record.to.update]\e[0m\n"
+echo -e "\e[1;39mExample: \e[1;36m$(basename ${0})" \
+    "\e[1;35m-f /home/janedoe/myCloudFlareDetails.info" \
+    "\e[1;33m-r server.mydomain.com\e[0m"
+echo -e "\e[1;39mExample: \e[1;36m$(basename ${0})" \
+    "\e[1;35m-f /home/janedoe/myCloudFlareDetails.info" \
+    "\e[1;33m-r server.mydomain.com\e[0m" \
+    "\e[1;33m-r server2.mydomain.com\e[0m"
+exit 1
+}
+
+### end of functions
+
+
+### unset environment variables used in this script and initialize arrays
+unset PARAMS
+unset accountFile
+dnsRecords=()
+
+### Process script parameters
+if [ -z $1 ]; then
+    scriptHelp
+fi
+while getopts ':f:r:' PARAMS; do
+    case "$PARAMS" in
+        f)
+            accountFile="${OPTARG}"
+            ;;
+        r)
+            dnsRecords+=($OPTARG)
+            ;;
+        ?)
+            scriptHelp            
+            ;;
+    esac
+done
+
+
+### Echo results (testing)
+echo "Based on parameters provided:"
+echo "Login details at: ${accountFile}"
+echo "Updating records: ${dnsRecords[*]}"
+
+exit 0
